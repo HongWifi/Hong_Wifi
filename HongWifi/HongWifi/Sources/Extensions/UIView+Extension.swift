a12729ec687e95e549431ce4be5e7f4d8300b6c2
wody27 <maxwodyd98@gmail.com>
2021-09-04 15:07:52 +0900

    :bug: gitignore 재적용

---
+++ HongWifi/HongWifi/Sources/Extensions/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  HongWifi
+//
+//  Created by Wody on 2021/09/04.
+//
+
+import Foundation
---
+++ HongWifi/HongWifi/Sources/Utils/HotspotManager.swift
@@ -0,0 +1,8 @@
+//
+//  HotspotManager.swift
+//  HongWifi
+//
+//  Created by Wody on 2021/09/04.
+//
+
+import Foundation
