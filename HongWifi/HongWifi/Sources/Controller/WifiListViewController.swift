172d661771ad13b145af1c52dafc712ef7e2a5a1
danhandev <danhandec@gmail.com>
2021-09-06 00:15:07 +0900

    FEAT: WifiListViewController, WIfiList storyboard

---
+++ HongWifi/HongWifi/Sources/Controller/WifiListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WifiListViewController.swift
+//  HongWifi
+//
+//  Created by danhan on 2021/09/05.
+//
+
+import Foundation
+import UIKit
+
+class WifiListViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
