fed74ebc50cbc25e7e8eb6b86077911bfdaa1438
M-22 <m-22@M-22ui-MacBookPro.local>
2021-09-11 09:55:22 +0900

    0911Done

---
+++ HongWifi/HongWifi/Sources/Controller/Popup2Controller.swift
@@ -0,0 +1,22 @@
+//
+//  Popup2Controller.swift
+//  HongWifi
+//
+//  Created by M-22 on 2021/09/11.
+//
+
+import UIKit
+
+class Popup2Controller: UIViewController {
+
+
+    @IBOutlet weak var savedWifiName: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
